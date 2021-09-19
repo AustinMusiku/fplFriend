1364a1a5d4c0eea557beeee23e91c73fd4ec38e1
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2021-09-19 13:07:05 +0300

    created graphql upcomingFixtures, graphql pastFixtures

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const { graphqlHTTP } = require('express-graphql')
-const { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLInt, GraphQLString } = require('graphql')
+const { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLInt, GraphQLString, GraphQLFloat, GraphQLBoolean } = require('graphql')
 
 const fetchMethods = require('../controllers/fetchControllers');
 
@@ -8,55 +8,271 @@ const PlayerType = new GraphQLObjectType({
     description: 'a player type',
     
     fields: () => ({
+        id: {
+            type: GraphQLInt,
+            resolve: player => player.id
+        },
         chance_of_playing_next_round: {
             type: GraphQLInt,
-            resolve: async (player) => {
-                chance_of_playing_next_round = await player.chance_of_playing_next_round
-                return chance_of_playing_next_round
-            }
+            resolve: player => player.chance_of_playing_next_round
         },
         cost_change_event: {
             type: GraphQLInt,
-            resolve: async (player) => {
-                cost_change_event = await player.cost_change_event;
-                return cost_change_event;
-            }
+            resolve: player => player.cost_change_event
         },
         element_type: {
             type: GraphQLInt,
-            resolve: async (player) => {
-                element_type = await player.element_type;
-                return element_type;
-            } 
+            resolve: player => player.element_type
         },
         event_points: {
             type: GraphQLInt,
-            resolve: async (player) => {
-                event_points = await player.event_points;
-                return event_points;
-            } 
+            resolve: player => player.event_points
         },
         first_name: {
             type: GraphQLString,
-            resolve: async (player) => {
-                first_name = await player.first_name;
-                return first_name;
-            } 
+            resolve: player => player.first_name
         },
         second_name: {
             type: GraphQLString,
-            resolve: async (player) => {
-                second_name = await player.second_name;
-                return second_name;
-            } 
+            resolve: player => player.second_name
         },
         web_name: {
             type: GraphQLString,
-            resolve: async (player) => {
-                web_name = await player.web_name;
-                return web_name;
-            } 
+            resolve: (player) => player.web_name
+        },
+        form: {
+            type: GraphQLFloat,
+            resolve: player => parseFloat(player.form) 
+        },
+        news: {
+            type: GraphQLString,
+            resolve: player => player.news
+        },
+        news_added: {
+            type: GraphQLString,
+            resolve: player => player.news_added
+        },
+        now_cost: {
+            type: GraphQLFloat,
+            resolve: player => player.now_cost/10
+        },
+        points_per_game: {
+            type: GraphQLFloat,
+            resolve: player => parseFloat(player.points_per_game)
+        },
+        selected_by_percent: {
+            type: GraphQLFloat,
+            resolve: player => parseFloat(player.selected_by_percent)
+        },
+        team: {
+            type: GraphQLInt,
+            resolve: player => parseInt(player.team)
+        },
+        total_points: {
+            type: GraphQLInt,
+            resolve: player => parseInt(player.total_points)
+        },
+        transfers_in_event: {
+            type: GraphQLInt,
+            resolve: player => parseInt(player.transfers_in_event)
+        },
+        transfers_out_event: {
+            type: GraphQLInt,
+            resolve: player => parseInt(player.transfers_out_event)
+        },
+        minutes: {
+            type: GraphQLInt,
+            resolve: player => parseInt(player.minutes)
+        },
+        goals_scored: {
+            type: GraphQLInt,
+            resolve: player => parseInt(player.goals_scored)
+        },
+        assists: {
+            type: GraphQLInt,
+            resolve: player => parseInt(player.assists)
+        },
+        saves: {
+            type: GraphQLInt,
+            resolve: player => parseInt(player.saves)
+        },
+        bonus: {
+            type: GraphQLInt,
+            resolve: player => parseInt(player.bonus)
+        },
+        bps: {
+            type: GraphQLInt,
+            resolve: player => parseInt(player.bps)
+        },
+        influence: {
+            type: GraphQLFloat,
+            resolve: player => parseFloat(player.influence)
+        },
+        creativity: {
+            type: GraphQLFloat,
+            resolve: player => parseFloat(player.creativity)
+        },
+        threat: {
+            type: GraphQLFloat,
+            resolve: player => parseFloat(player.threat)
+        },
+        ict_index: {
+            type: GraphQLFloat,
+            resolve: player => parseFloat(player.ict_index)
+        },
+        upcomingFixtures: {
+            type: GraphQLList(UpcomingFixtureType),
+            args: {
+                id: {
+                    type: GraphQLInt
+                }
+            },
+            resolve: async (args) => {
+                let playerUpcomingFixtures = await fetchMethods.getPlayerEventsById(args.id);
+                console.log(playerUpcomingFixtures.fixtures.length)
+                return playerUpcomingFixtures.fixtures
+            }
+        },
+        pastFixtures: {
+            type: GraphQLList(PastFixtureType),
+            args: {
+                id: {
+                    type: GraphQLInt
+                }
+            },
+            resolve: async (args) => {
+                let playerEvents = await fetchMethods.getPlayerEventsById(args.id)
+                return playerEvents.history
+            }
         }
+    })
+})
+
+const UpcomingFixtureType = new GraphQLObjectType({
+    name: 'upcomingFixture',
+    description: 'upcoming fixture',
+
+    fields: () => ({
+        id: {
+            type: GraphQLInt,
+            resolve: upcomingFixture => upcomingFixture.id
+        },
+        event: {
+            type: GraphQLInt,
+            resolve: upcomingFixture => upcomingFixture.event
+        },
+        minutes: {
+            type: GraphQLInt,
+            resolve: upcomingFixture => upcomingFixture.minutes
+        },
+        difficulty: {
+            type: GraphQLInt,
+            resolve: upcomingFixture => upcomingFixture.difficulty
+        },
+        team_h: {
+            type: GraphQLInt,
+            resolve: upcomingFixture => upcomingFixture.team_h
+        },
+        team_a: {
+            type: GraphQLInt,
+            resolve: upcomingFixture => upcomingFixture.team_a
+        },
+        finished: {
+            type: GraphQLBoolean,
+            resolve: upcomingFixture => upcomingFixture.finished
+        },
+        is_home: {
+            type: GraphQLBoolean,
+            resolve: upcomingFixture => upcomingFixture.is_home
+        }
+    })
+})
+
+const PastFixtureType = new GraphQLObjectType({
+    name: 'PastFixture',
+    description: 'Past fixtures',
+
+    fields: () => ({
+        was_home: {
+            type: GraphQLBoolean,
+            resolve: PastFixture => PastFixture.was_home
+        },
+        opponent_team: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.opponent_team
+        },
+        total_points: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.total_points
+        },
+        team_h_score: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.team_h_score
+        },
+        team_a_score: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.team_a_score
+        },
+        round: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.round
+        },
+        minutes: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.minutes
+        },
+        goals_scored: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.goals_scored
+        },
+        assists: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.assists
+        },
+        clean_sheets: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.clean_sheets
+        },
+        goals_conceded: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.goals_conceded
+        },
+        own_goals: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.own_goals
+        },
+        penalties_saved: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.penalties_saved
+        },
+        penalties_missed: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.penalties_missed
+        },
+        yellow_cards: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.yellow_cards
+        },
+        red_cards: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.red_cards
+        },
+        saves: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.saves
+        },
+        bps: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.bps
+        },
+        value: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.value
+        },
+        selected: {
+            type: GraphQLInt,
+            resolve: PastFixture => PastFixture.selected
+        },
     })
 })
 
