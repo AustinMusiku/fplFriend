5e5eab6fd3a71ceabb811ada9f94b3080b7f071a
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2021-09-23 03:40:02 +0300

    setup gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, watch, series } = require('gulp');
+const terser = require('gulp-terser');
+const minify = require('gulp-clean-css');
+const sass = require('gulp-sass')(require('sass'));
+const prefix = require('gulp-autoprefixer');
+
+
+// compile sass
+const compileSass = () => {
+    return src('./public/scss/style.scss')
+        .pipe(sass())
+        .pipe(prefix())
+        .pipe(minify())
+        .pipe(dest('dist/css'))
+}
+
+// minify js
+const minifyJs = () => {
+    return src('./public/js/*.js')
+        .pipe(terser())
+        .pipe(dest('dist/js'))
+}
+
+// watch task
+const watchTask = () => {
+    watch('./public/scss/style.css', compileSass)
+    watch('./public/js/*.js', minifyJs)
+}
+
+// default gulp
+exports.default = series( compileSass, minifyJs, watchTask );
