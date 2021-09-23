c45033c8e8bac433b99974f792e1e7a98b6c1516
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2021-09-23 03:52:16 +0300

    fixed dist assets urls

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express()
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/public', express.static(path.join(__dirname + '/public')))
+app.use('/dist', express.static(path.join(__dirname + '/dist')))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname + '/views'))
 app.use(cors());
