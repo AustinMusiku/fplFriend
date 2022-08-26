59995b4b80e9b43a86dd750448c69292aaff8cd6
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-08-26 05:17:43 +0300

    fix: team codes revision

--- public/js/index.js
+++ public/js/index.js
@@ -46,7 +46,7 @@ document.addEventListener('scroll', () => {
 // evaluation methods
 const evaluatePosition = (positionId) => { switch (positionId) { case 1: return 'goalkeeper'; case 2: return 'defender'; case 3: return 'midfielder'; case 4: return 'forward'; default: return 'n/a'; } }
 
-const evaluateTeam = (teamId) => { switch (teamId) { case 1:return 'ars'; case 2:return 'avl'; case 3:return 'bre'; case 4:return 'bha'; case 5:return 'bur'; case 6:return 'che'; case 7:return 'cry'; case 8:return 'eve'; case 9:return 'lei'; case 10:return 'lee'; case 11:return 'liv'; case 12: return 'mci'; case 13: return 'mun'; case 14: return 'new'; case 15: return 'nor'; case 16: return 'sou'; case 17: return 'tot'; case 18: return 'wat'; case 19: return 'whu'; case 20: return 'wol'; default: return 'n/a'; } }
+const evaluateTeam = (teamId) => { switch (teamId) { case 1:return 'ars'; case 2:return 'avl'; case 3:return 'bou'; case 4:return 'bre';case 5:return 'bha'; case 6:return 'che'; case 7:return 'cry'; case 8:return 'eve'; case 9:return 'ful'; case 10:return 'lei'; case 11:return 'lee'; case 12: return 'liv'; case 13: return 'mci'; case 14: return 'mun'; case 15: return 'new'; case 16: return 'nfo'; case 17: return 'sou'; case 18: return 'tot'; case 19: return 'whu'; case 20: return 'wol'; default: return 'n/a'; } }
 
 const evalutePriceChange = (priceChange) => { if(priceChange == 1){ return 'rise' }  if(priceChange == 0){ return 'equal' }  if(priceChange == -1){ return 'fall' } }
 
